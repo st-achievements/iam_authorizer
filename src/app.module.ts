96e43dae21d826e271e318f9000b4827350158bd
stLmpp <gui.stlmpp@gmail.com>
2024-05-18 00:00:43 -0300

    - remove firebase dependency

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { DrizzleOrmModule } from '@st-achievements/database';
 import { CoreModule } from '@st-api/core';
-import { FirebaseAdminModule, FirebaseModule } from '@st-api/firebase';
+import { FirebaseAdminModule } from '@st-api/firebase';
 
 @Module({
   imports: [
     CoreModule.forRoot(),
-    FirebaseModule.forRoot(),
     FirebaseAdminModule.forRoot(),
     DrizzleOrmModule,
   ],
